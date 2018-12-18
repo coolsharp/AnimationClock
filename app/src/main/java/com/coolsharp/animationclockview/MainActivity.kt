7d0a224b0e082d49e7594efc5ea050cb6bda1ba3
coolsharp <coolsharp@wemakeprice.com>
2018-12-18 17:59:36 +0900

    셈플 수정

--- app/src/main/java/com/coolsharp/animationclockview/MainActivity.kt
+++ app/src/main/java/com/coolsharp/animationclockview/MainActivity.kt
@@ -2,15 +2,30 @@ package com.coolsharp.animationclockview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.coolsharp.animationclock.ClockView
 
 class MainActivity : AppCompatActivity() {
+
+    var milisecond = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var clockView = findViewById<ClockView>(R.id.clockView)
 
-        clockView.setTime(60)
+        var milisecond = 60
+
+        object : CountDownTimer(60000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                clockView.setTime(milisecond)
+                milisecond--
+            }
+
+            override fun onFinish() {
+                clockView.setTime(0)
+            }
+        }.start()
     }
 }
